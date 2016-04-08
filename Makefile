6b62c5a00937de7fdfe91e77a405b3ef1a0be07f
Paul I Wilson <paul.i.wilson@ft.com>
2016-04-08 15:28:13 +0100

    adding backend json to test

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ parser:
 	pegjs vcl.peg vcl-parser.js
 
 test: parser
-	node --harmony_rest_parameters test.js --src test.vcl
+	node --harmony_rest_parameters test.js --src test.vcl --backends
--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 'use strict';
 const parser = require('./vcl-parser');
 const fs = require('fs');
-const args = require('minimist')(process.argv.slice(2));
+const args = require('minimist')(process.argv.slice(2), {
+	alias : {'backends' : 'b'},
+	default : {'backends' : false}
+});
+
+console.log(args);
+
+
 const colors = require('colors');
 const util = require('util');
 
@@ -36,11 +43,55 @@ for(let a of logColors){
 
 let src = fs.readFileSync(args.src, {encoding:'utf8'});
 
+function valueAsJson(value){
+	if(value[0] === 'Duration'){
+		return value[1] + value[2];
+	}
+
+	if(value[0] === 'StringConcatenation'){
+		return value[1].reduce((p, c) =>  p + c[1] + ' ', '').trim();
+	}
+
+	return value[1];
+}
+
+function getBackendsAsJson(result){
+	let json = {backends:[], healthchecks:[]};
+	for(let r of result){
+		if(r[0] === 'Backend'){
+			let backend = {name:r[1].name};
+			for(let p of r[1].properties){
+				if(p[0] === 'Healthcheck'){
+					let healthcheck = {backend : backend.name};
+					for(let h of p[1]){
+						healthcheck[h[1].name] = valueAsJson(h[1].value);
+					}
+					json.healthchecks.push(healthcheck);
+				}else if(p[0] === 'SetBackendProperty'){
+					backend[p[1].name] = valueAsJson(p[1].value);
+				}
+			}
+			json.backends.push(backend);
+		}
+	}
+
+	return json;
+}
+
+function onSuccess(result){
+	if(args.backends){
+		log.success(getBackendsAsJson(result));
+	}else{
+		log.success(result);
+	}
+
+}
+
 let result;
 
 try{
 	result = parser.parse(src);
-	log.success(result);
+	onSuccess(result);
 }catch(e){
 	if(e.name === 'SyntaxError') {
 		log.error(`Syntax Error! line ${e.location.start.line} column ${e.location.start.column}: ${e.message}`);
